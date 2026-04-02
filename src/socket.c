c66d88100c28ecf36bb09e10cb56f79a7af5071b
24kimel <itai.kimelman@gmail.com>
2026-04-02 10:31:31 +0300

    fix(pre-commit): Change style to LLVM

--- src/socket.c
+++ src/socket.c
@@ -8,44 +8,35 @@
 #include "buffer.h"
 
 // TODO: return status code
-void
-tcp_socket_init (int *sockfd)
-{
-  int status = 0;
+void tcp_socket_init(int *sockfd) {
+    int status = 0;
 
-  *sockfd = socket (AF_INET, SOCK_RAW, IPPROTO_TCP);
-  if (-1 == *sockfd)
-    {
-      perror ("socket");
+    *sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    if (-1 == *sockfd) {
+        perror("socket");
     }
 }
 
-void
-tcp_socket_send (int sockfd, uint32_t ip_address, buffer_t buffer)
-{
-  int status = 0;
-  struct sockaddr_in addr = { 0 };
+void tcp_socket_send(int sockfd, uint32_t ip_address, buffer_t buffer) {
+    int status = 0;
+    struct sockaddr_in addr = {0};
 
-  addr.sin_addr.s_addr = (in_addr_t)(htonl (ip_address));
-  addr.sin_port = 0;
-  addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = (in_addr_t)(htonl(ip_address));
+    addr.sin_port = 0;
+    addr.sin_family = AF_INET;
 
-  status = sendto (sockfd, buffer.data, buffer.len, 0,
-                   (struct sockaddr *)(&addr), sizeof (addr));
-  if (-1 == status)
-    {
-      perror ("sendto");
+    status = sendto(sockfd, buffer.data, buffer.len, 0,
+                    (struct sockaddr *)(&addr), sizeof(addr));
+    if (-1 == status) {
+        perror("sendto");
     }
 }
 
-void
-tcp_socket_destroy (int sockfd)
-{
-  int status = 0;
+void tcp_socket_destroy(int sockfd) {
+    int status = 0;
 
-  status = close (sockfd);
-  if (-1 == status)
-    {
-      perror ("close");
+    status = close(sockfd);
+    if (-1 == status) {
+        perror("close");
     }
 }
